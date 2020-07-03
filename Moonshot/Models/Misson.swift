b0b7fc368e6a3ac98f0de81a145bc5ccf6fe999d
Chris Eadie <chris.eadie@outlook.com>
2020-07-03 11:57:25 +0100

    Add Mission model

---
+++ Moonshot/Models/Misson.swift
@@ -0,0 +1,20 @@
+//
+//  Misson.swift
+//  Moonshot
+//
+//  Created by Chris Eadie on 03/07/2020.
+//
+
+import Foundation
+
+struct Misson: Codable, Identifiable {
+  struct CrewPerson: Codable {
+    let name: String
+    let role: String
+  }
+  
+  let id: Int
+  let launchdate: String?
+  let crew: [CrewPerson]
+  let description: String
+}
