1162bf562b77b7eaf743a8175426db7303afda5b
Chris Eadie <chris.eadie@outlook.com>
2020-07-05 14:07:52 +0100

    Filled out AstronautView

--- Moonshot/Views/AstronautView.swift
+++ Moonshot/Views/AstronautView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct AstronautView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  
+  let astronaut: Astronaut
+  
+  var body: some View {
+    GeometryReader { geometry in
+      ScrollView(.vertical) {
+        VStack {
+          Image(self.astronaut.id)
+            .resizable()
+            .scaledToFit()
+            .frame(width: geometry.size.width)
+          Text(self.astronaut.description)
+            .padding()
+        }
+      }
     }
+    .navigationBarTitle(Text(astronaut.name), displayMode: .inline)
+  }
+  
 }
 
 struct AstronautView_Previews: PreviewProvider {
-    static var previews: some View {
-        AstronautView()
-    }
+  static let astronaut = Astronauts().list[(Int.random(in: 0..<Astronauts().list.count))]
+  
+  static var previews: some View {
+    AstronautView(astronaut: astronaut)
+  }
 }
