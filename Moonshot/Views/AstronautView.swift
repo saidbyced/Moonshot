bab6f80db53978235742925ac2a732c179cb1962
Chris Eadie <chris.eadie@outlook.com>
2020-07-05 13:51:51 +0100

    Add AstronautView file

---
+++ Moonshot/Views/AstronautView.swift
@@ -0,0 +1,20 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Chris Eadie on 05/07/2020.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static var previews: some View {
+        AstronautView()
+    }
+}
